4cd3b7c6a050fa3bec853b47d463b9a90c3a4283
toshith-developer <57741949+toshith-developer@users.noreply.github.com>
2020-05-29 09:38:08 +0530

    monitor mode sh script

---
+++ Kali pi/dependencies.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install ettercap-text-only
+sudo apt install iptables
+sudo apt install dsniff
+sudo apt install nmap
+sudo chmod +x monitor_mode.sh
+./monitor_mode.sh
---
+++ Kali pi/packet_capturing.sh
@@ -0,0 +1,2 @@
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+sudo ettercap -Tq -i wlan0 -w ~/Desktop/cap
